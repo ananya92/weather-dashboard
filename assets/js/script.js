0bc2449ddc8429c1bf0c8a1985c4c22fcc118271
ananya92 <1992ananya@gmail.com>
2019-12-04 22:01:50 +1030

    Read the weather parameter from the response from the current weather API and displayed them on the current weather div

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var apikey = "&appid=c63725fc3433c437d23d3a51038313a5"
 
 //Variable storing openweather API url
 var queryUrl = "https://api.openweathermap.org/data/2.5/weather?";
-
+var queryUrlUVIndex = "https://api.openweathermap.org/data/2.5/uvi?";
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showWeatherAtPosition);
@@ -27,7 +27,35 @@ function showWeatherAtPosition(position) {
             url: queryUrl + "lat=" + latitude + "&lon=" + longitude + apikey,
             method: "GET"
         }).then(function(response) {
+            var currentTime = moment().format('l');
+            //Variable storing the weather icon link
+            var iconurl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+            $("#currentWeather").attr("style", "display: block;");
             console.log(response);
+            var nameTag = $("#name");
+            var dateTag = $("#date");
+            var iconTag = $("#weatherIcon");
+            var descTag = $("#desc");
+            var tempTag = $("#temperature");
+            var humidTag = $("#humidity");
+            var windTag = $("#wSpeed");
+            nameTag.text(response.name);
+            nameTag.attr("style", "font-size: 22px; font-weight: bold;");
+            dateTag.text("(" + currentTime + ")");
+            dateTag.attr("style", "font-size: 22px; font-weight: bold;");
+            iconTag.attr("src", iconurl);
+            iconTag.attr("height", "60px");
+            iconTag.attr("style", "padding-bottom: 5px;");
+            descTag.text(response.weather[0].main);
+            descTag.attr("style", "font-size: 22px;");
+            //converting temperature from Kelvin to Celcius
+            var tempC = parseInt(response.main.temp) - 273.15 ;
+            tempC = tempC.toFixed(2);
+            tempTag.text(tempTag.text() + tempC + " \u2103");
+            humidTag.text(humidTag.text() + response.main.humidity + " %");
+            //Converting speed from meter per second to miles per hour
+            var speedMPH = (parseInt(response.wind.speed) * 2.23694);      
+            windTag.text(windTag.text() + speedMPH.toFixed(2) + " MPH");
         });
     }
 }
